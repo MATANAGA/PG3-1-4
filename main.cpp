8c1993ddf1e3665030535701f9e7b7f9a538acb4
YONGFA <k024g1037@g.neec.ac.jp>
2025-06-20 14:13:35 +0900

    Q

--- main.cpp
+++ main.cpp
@@ -1,54 +1,37 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
-// クラステンプレート定義
-template <typename Type1, typename Type2>
-class TemplateClass {
+// 動物クラス（抽象クラス）
+class Animal {
 public:
-    // コンストラクタ
-    TemplateClass(Type1 number1, Type2 number2)
-        : Number1(number1), Number2(number2) {}
+    virtual void sound() const = 0; // 純粋仮想関数（ポリモーフィズムのため）
+    virtual ~Animal() {}            // 仮想デストラクタ
+};
 
-    // 小さい方の値を返す
-    Type1 Min() {
-        if (Number1 < Number2)
-            return static_cast<Type1>(Number1);
-        else
-            return static_cast<Type1>(Number2);
+// 犬クラス
+class Dog : public Animal {
+public:
+    void sound() const override {
+        printf("イヌ鳴き声：ワン\n");
     }
+};
 
-private:
-    Type1 Number1;
-    Type2 Number2;
+// 猫クラス
+class Cat : public Animal {
+public:
+    void sound() const override {
+        printf("ネコ鳴き声:ニャー\n");
+    }
 };
 
 int main() {
-    // 各パターンのインスタンス生成
-    TemplateClass<int, float> intFloatTemplate(100, 50.0f);
-    TemplateClass<int, double> intDoubleTemplate(80, 20.0);
-    TemplateClass<float, int> floatIntTemplate(2.0f, 18);
-    TemplateClass<float, double> floatDoubleTemplate(11.0f, 3.5);
-    TemplateClass<double, int> doubleIntTemplate(234.0, 123);
-    TemplateClass<double, float> doubleFloatTemplate(400.1, 500.2f);
+    Animal* animal1 = new Dog(); // 犬インスタンス
+    Animal* animal2 = new Cat(); // 猫インスタンス
 
-    // Min関数の実行と表示
-    cout << "int(100) と float(50.0f) を比べて小さい方: "
-        << intFloatTemplate.Min() << endl;
+    animal1->sound(); // ポリモーフィズムにより Dog::sound() 呼び出し
+    animal2->sound(); // 同様に Cat::sound() 呼び出し
 
-    cout << "int(80) と double(20.0) を比べて小さい方: "
-        << intDoubleTemplate.Min() << endl;
-
-    cout << "float(2.0f) と int(18) を比べて小さい方: "
-        << floatIntTemplate.Min() << endl;
-
-    cout << "float(11.0f) と double(3.5) を比べて小さい方: "
-        << floatDoubleTemplate.Min() << endl;
-
-    cout << "double(234.0) と int(123) を比べて小さい方: "
-        << doubleIntTemplate.Min() << endl;
-
-    cout << "double(400.1) と float(500.2f) を比べて小さい方: "
-        << doubleFloatTemplate.Min() << endl;
+    delete animal1;
+    delete animal2;
 
     return 0;
 }
